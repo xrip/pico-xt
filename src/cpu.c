2989a4640c7cc4e32aea48d110de8a5d0254777e
Mike Vengerovich <mikeveng73@gmail.com>
2023-11-21 04:25:14 +0200

    + Expanden memory management

--- src/cpu.c
+++ src/cpu.c
@@ -192,14 +192,18 @@ __inline void write86(uint32_t addr32, uint8_t value) {
             return;
         }
 #endif
-        addr32 -= 0xCC000UL;
+        addr32 -= 0xCC000UL; // TODO: why?
     }
     else if ((addr32) > 0xFFFFFUL) {
 #if SD_CARD_SWAP
-        if (addr32 < (0xFFFFFUL + (64UL << 10)) && (portin(PORT_A20) & A20_ENABLE_BIT) != 0) { // A20 line is ON
+        if (addr32 >= 0x100000UL && addr32 <= 0xFFFF0UL + 0xFFFFUL) { // Hihg mem (1Mb + 64Kb)
+          if ((portin(PORT_A20) & A20_ENABLE_BIT) != 0) { // A20 line is ON
             char tmp[40]; sprintf(tmp, "HIMEM W LBA: 0x%X", addr32); logMsg(tmp);
             ram_page_write(addr32, value);
+          }
         }
+        write86(addr32 - 0x100000UL, value);
+        return;
 #endif
 #if PICO_ON_DEVICE
     } else if (PSRAM_AVAILABLE) {
@@ -328,17 +332,17 @@ __inline uint8_t read86(uint32_t addr32) {
         addr32 -= 0xB8000UL;
         return VRAM[addr32]; //
     }
-    else if ((addr32 >= 0xD0000UL) && (addr32 < 0xD8000UL)) {
+    else if ((addr32 >= 0xD0000UL) && (addr32 < 0xD8000UL)) { // Expanded memory paging space D00000-E00000
         // NE2000 ??? or EMM
 #if SD_CARD_SWAP
-// TODO: PSRAM_AVAILABLE ...
+                             // TODO: PSRAM_AVAILABLE ...
         if ((portin(PORT_A20) & A20_ENABLE_BIT) != 0) {
             char tmp[40]; sprintf(tmp, "0xD0000 LBA: 0x%X", addr32); logMsg(tmp);
             const uint32_t ram_page = get_ram_page_for(addr32);
             const uint32_t addr_in_page = addr32 & RAM_IN_PAGE_ADDR_MASK;
             return RAM[(ram_page * RAM_PAGE_SIZE) + addr_in_page];
         }
-        addr32 -= 0xCC000UL;
+        addr32 -= 0xCC000UL; // TODO: why?
 #endif
     }
     else if ((addr32 >= 0xF6000UL) && (addr32 < 0xFA000UL)) {
@@ -351,13 +355,16 @@ __inline uint8_t read86(uint32_t addr32) {
         addr32 -= 0xFA000UL;
         return BASICH[addr32];
     }
-    else if (addr32 > 0xFFFFFUL && addr32 < 0xFFFFFUL + (64UL << 10) && (portin(PORT_A20) & A20_ENABLE_BIT) != 0) {
+    else if (addr32 >= 0x100000UL && addr32 <= 0xFFFF0UL + 0xFFFFUL) { // Hihg mem (1Mb + 64Kb)
 #if SD_CARD_SWAP
 // TODO: PSRAM_AVAILABLE ...
-        char tmp[40]; sprintf(tmp, "HIMEM LBA: 0x%X", addr32); logMsg(tmp);
-        const uint32_t ram_page = get_ram_page_for(addr32);
-        const uint32_t addr_in_page = addr32 & RAM_IN_PAGE_ADDR_MASK;
-        return RAM[(ram_page * RAM_PAGE_SIZE) + addr_in_page];
+        if ((portin(PORT_A20) & A20_ENABLE_BIT) != 0) {
+            char tmp[40]; sprintf(tmp, "HIMEM LBA: 0x%X", addr32); logMsg(tmp);
+            const uint32_t ram_page = get_ram_page_for(addr32);
+            const uint32_t addr_in_page = addr32 & RAM_IN_PAGE_ADDR_MASK;
+            return RAM[(ram_page * RAM_PAGE_SIZE) + addr_in_page];
+        }
+        return read86(addr32 - 0x100000UL); // FFFF:0010 -> 0000:0000 rolling address space for case A20 is turned off
 #endif
     }
 #if PICO_ON_DEVICE
@@ -819,7 +826,122 @@ uint32_t BlinkTimer(uint32_t interval, void *name) {
 #include <hardware/flash.h>
 #endif
 
-int e820_count;
+// Maximum number of map entries in the e820 map
+#define BUILD_MAX_E820 32
+int e820_count = 0;
+struct e820entry e820_list[BUILD_MAX_E820];
+
+// Remove an entry from the e820_list.
+static void remove_e820(int i) {
+    e820_count--;
+    memmove(&e820_list[i], &e820_list[i+1], sizeof(e820_list[0]) * (e820_count - i));
+}
+
+// Insert an entry in the e820_list at the given position.
+static void insert_e820(int i, uint64_t start, uint64_t size, uint32_t type) {
+    if (e820_count >= BUILD_MAX_E820) {
+        warn_noalloc();
+        return;
+    }
+    memmove(&e820_list[i+1], &e820_list[i], sizeof(e820_list[0]) * (e820_count - i));
+    e820_count++;
+    struct e820entry *e = &e820_list[i];
+    e->start = start;
+    e->size = size;
+    e->type = type;
+}
+
+static const char* e820_type_name(uint32_t type) {
+    switch (type) {
+    case E820_RAM:      return "RAM";
+    case E820_RESERVED: return "RESERVED";
+    case E820_ACPI:     return "ACPI";
+    case E820_NVS:      return "NVS";
+    case E820_UNUSABLE: return "UNUSABLE";
+    default:            return "UNKNOWN";
+    }
+}
+
+// Show the current e820_list.
+static void dump_map(void) {
+    dprintf(1, "e820 map has %d items:\n", e820_count);
+    for (int i = 0; i < e820_count; i++) {
+        struct e820entry *e = &e820_list[i];
+        uint64_t e_end = e->start + e->size;
+        dprintf(1, "  %d: %016llx - %016llx = %d %s\n", i, e->start, e_end, e->type, e820_type_name(e->type));
+    }
+}
+
+#define E820_HOLE ((uint32_t)-1) // Used internally to remove entries
+
+// Add a new entry to the list.  This scans for overlaps and keeps the
+// list sorted.
+void e820_add(uint64_t start, uint64_t size, uint32_t type) {
+    dprintf(8, "Add to e820 map: %08llx %08llx %d\n", start, size, type);
+    if (! size)
+        // Huh?  Nothing to do.
+        return;
+    // Find position of new item (splitting existing item if needed).
+    uint64_t end = start + size;
+    int i;
+    for (i = 0; i < e820_count; i++) {
+        struct e820entry *e = &e820_list[i];
+        uint64_t e_end = e->start + e->size;
+        if (start > e_end)
+            continue;
+        // Found position - check if an existing item needs to be split.
+        if (start > e->start) {
+            if (type == e->type) {
+                // Same type - merge them.
+                size += start - e->start;
+                start = e->start;
+            } else {
+                // Split existing item.
+                e->size = start - e->start;
+                i++;
+                if (e_end > end)
+                    insert_e820(i, end, e_end - end, e->type);
+            }
+        }
+        break;
+    }
+    // Remove/adjust existing items that are overlapping.
+    while (i < e820_count) {
+        struct e820entry *e = &e820_list[i];
+        if (end < e->start)
+            // No overlap - done.
+            break;
+        uint64_t e_end = e->start + e->size;
+        if (end >= e_end) {
+            // Existing item completely overlapped - remove it.
+            remove_e820(i);
+            continue;
+        }
+        // Not completely overlapped - adjust its start.
+        e->start = end;
+        e->size = e_end - end;
+        if (type == e->type) {
+            // Same type - merge them.
+            size += e->size;
+            remove_e820(i);
+        }
+        break;
+    }
+    // Insert new item.
+    if (type != E820_HOLE)
+        insert_e820(i, start, size, type);
+    //dump_map();
+}
+
+// Remove any definitions in a memory range (make a memory hole).
+void e820_remove(uint64_t start, uint64_t size) {
+    e820_add(start, size, E820_HOLE);
+}
+
+// Report on final memory locations.
+void e820_prepboot(void) {
+    dump_map();
+}
 
 void reset86() {
 #if !PICO_ON_DEVICE
@@ -905,20 +1027,26 @@ void intcall86(uint8_t intnum) {
                 case 0x24: 
                     switch(CPU_AL) {
                         case 0x00:
-                            CPU_AH = 0;
                             portout16(PORT_A20, portin(PORT_A20) | A20_ENABLE_BIT);
+                            cf = 0; CPU_AH = 0;
+                            logMsg("INT15! 2400 |A20_ENABLE_BIT");
                             break;
                         case 0x01:
                             portout16(PORT_A20, portin(PORT_A20) & ~A20_ENABLE_BIT);
-                            CPU_AH = 0;
+                            cf = 0; CPU_AH = 0;
+                            logMsg("INT15! 2401 ~A20_ENABLE_BIT");
                             break;
                         case 0x02:
                             CPU_AL = (portin(PORT_A20) & A20_ENABLE_BIT) != 0;
-                            CPU_AH = 0;
+                            cf = 0; CPU_AH = 0;{
+                                char tmp[80]; sprintf(tmp, "INT15! 2402 AL: 0x%X", CPU_AL); logMsg(tmp);
+                            }
                             break;
                         case 0x03:
                             CPU_BX = 3;
                             CPU_AH = 0;
+                            cf = 0;
+                            logMsg("INT15! 2403 BX: 3");
                             break;
                         default:
                             CPU_AH = 0x86;
@@ -932,6 +1060,7 @@ void intcall86(uint8_t intnum) {
                 case 0x52: // removable media eject
                     // TODO:
                     CPU_AH = 0;
+                    cf = 0;
                     break;
                 case 0x53: // APM
                     // TODO:
@@ -948,7 +1077,14 @@ void intcall86(uint8_t intnum) {
                     // TODO:
                     break;
                 case 0x88:
-                    CPU_AX = EXPANDED_MEMORY_KBS - 1;
+                    if (EXPANDED_MEMORY_KBS > 64 * 1024) {
+                        CPU_AX = 63 * 1024;
+                    } else {
+                        CPU_AX = EXPANDED_MEMORY_KBS - 1024;
+                    }{
+                       char tmp[80]; sprintf(tmp, "INT15! 88 AX: 0x%X", CPU_AX); logMsg(tmp);
+                    }
+                    cf = 0;
                     break;
                 case 0x89: // switch to protected mode 286+
                     break;
@@ -956,45 +1092,52 @@ void intcall86(uint8_t intnum) {
                     break;
                 case 0x91: // Interrupt complete.  Called by Int 16h when key becomes available
                     break;
-                case 0xC0:
-                    CPU_AH = 0;{
-                    char tmp[80]; sprintf(tmp, "INT 15h ?? CPU_AH: 0x%X; CPU_AL: 0x%X", CPU_AH, CPU_AL); logMsg(tmp);
-                }
+                case 0xC0: // TODO: ensure
                     CPU_ES = 0xF000; // BIOS segment
                     CPU_BX = 0xE6f5; // BIOS config table
+                    CPU_AH = 0; {
+                       char tmp[80]; sprintf(tmp, "INT 15h C0h ES: 0x%X; BX: 0x%X", CPU_ES, CPU_BX); logMsg(tmp);
+                    }
                     break;
                 case 0xE8:
                     switch(CPU_AL) {
                         case 0x01:
                             if (EXPANDED_MEMORY_KBS > 16*1024) {
-                                CPU_CX = 1024*15; // 16MB
-                                CPU_DX = (EXPANDED_MEMORY_KBS - 16*1024) / 64;
+                                CPU_CX = 1024 * 15; // 15MB
+                                CPU_DX = (EXPANDED_MEMORY_KBS - 16 * 1024) / 64;
                             } else {
                                 CPU_CX = EXPANDED_MEMORY_KBS - 1;
                                 CPU_DX = 0;
                             }
                             CPU_AX = CPU_CX;
-                            CPU_BX = CPU_DX;{
-                    char tmp[80]; sprintf(tmp, "INT 15h !! CPU_AH: 0x%X; CPU_AL: 0x%X", CPU_AH, CPU_AL); logMsg(tmp);
-                }
+                            CPU_BX = CPU_DX; {
+                                char tmp[80]; sprintf(tmp, "INT15! E8AX: 0x%X; BX: 0x%X", CPU_AX, CPU_BX); logMsg(tmp);
+                            }
+                            cf = 0;
                             break;
                         case 0x20: {
+                            // ES:DI - destination for the table
                                 int count = e820_count;
-                                if (CPU_BX >= count || CPU_CX < sizeof(e820_list[0])) {
+                                if (CPU_DX != 0x534D4150 || CPU_BX >= count || CPU_CX < sizeof(e820_list[0])) {
                                     CPU_AH = 0x86;
                                     cf = 1;
+                                    logMsg("INT15! E820 failed");
                                     return;
-                               }
-/* TODO:
-    memcpy_far(regs->es, (void*)(regs->di+0)
-               , get_global_seg(), &e820_list[regs->bx]
-               , sizeof(e820_list[0]));*/
+                                }
+                                memcpy_far(CPU_ES,
+                                           (void*)(CPU_DI + 0),
+                                           get_global_seg(),
+                                           &e820_list[CPU_BX],
+                                           sizeof(e820_list[0])
+                                );
                                 if (CPU_BX == count - 1)
                                     CPU_BX = 0;
                                 else
                                     CPU_BX++;
                                 CPU_AX = 0x534D4150;
                                 CPU_CX = sizeof(e820_list[0]);
+                                char tmp[80]; sprintf(tmp, "INT15! E820 CX: 0x%X; BX: 0x%X", CPU_CX, CPU_BX); logMsg(tmp);
+                                cf = 0;
                             }
                             break;
                         default:
