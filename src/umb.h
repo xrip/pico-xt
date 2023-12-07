8812eee7cd5f78070939861e9709f5b933738e18
Mike Vengerovich <mikeveng73@gmail.com>
2023-12-07 03:05:24 +0200

    + size of UMB page was corrected

--- src/umb.h
+++ src/umb.h
@@ -5,7 +5,7 @@
 #ifdef XMS_UMB
   #define UMB_START_ADDRESS 0xC0000ul
   #define UMB_BLOCKS 2
-  #define RESERVED_XMS_KB (UMB_BLOCKS * 16 + 64)
+  #define RESERVED_XMS_KB (UMB_BLOCKS * 32 + 64)
 #else
   #define RESERVED_XMS_KB 0
 #endif
