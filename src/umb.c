72a946acacfd332bdc85fbd352c6239840e5c42a
Mike Vengerovich <mikeveng73@gmail.com>
2023-12-08 23:47:20 +0200

    6 UMB

--- src/umb.c
+++ src/umb.c
@@ -15,8 +15,7 @@ static umb_t umb_blocks[UMB_BLOCKS] = {
     0xE000, 0x0800, false,
     0xE800, 0x0800, false,
     0xF000, 0x0800, false,
-   // 0xF800, 0x0600, false // 24k before BIOS
-    // TODO: over BASIC
+    0xF800, 0x0600, false // 24k before BIOS
 };
 
 void init_umb() {
@@ -30,86 +29,54 @@ void init_umb() {
 }
 
 static void write8umb_psram(uint32_t addr32, uint8_t v) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         write8psram(addr32, v);
     }
 }
 
 static void write8umb_swap(uint32_t addr32, uint8_t v) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         ram_page_write(addr32, v);
     }
 }
 static void write16umb_psram(uint32_t addr32, uint16_t v) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         write16psram(addr32, v);
     }
 }
 
-static inline uint8_t read86rom(uint32_t addr32) {
-    if ((addr32 >= 0xFE000UL) && (addr32 <= 0xFFFFFUL)) {
-        // BIOS ROM range
-        return *(pBIOS() + addr32 - 0xFE000UL);
-    }
-    if ((addr32 >= 0xF6000UL) && (addr32 < 0xFA000UL)) {
-        // IBM BASIC ROM LOW
-        return *(pBASICL + addr32 - 0xF6000UL);
-    }
-    if ((addr32 >= 0xFA000UL) && (addr32 < 0xFE000UL)) {
-        // IBM BASIC ROM HIGH
-        return *(pBASICH + addr32 - 0xFA000UL);
-    }
-    return 0;
-}
-
 static void write16umb_swap(uint32_t addr32, uint16_t v) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         ram_page_write16(addr32, v);
     }
 }
 
 static uint8_t read8umb_psram(uint32_t addr32) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         return read8psram(addr32);
     }
-    return read86rom(addr32);
+    return *(pBIOS() + addr32 - 0xFE000UL);
 }
 
 static uint8_t read8umb_swap(uint32_t addr32) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         return ram_page_read(addr32);
     }
-    return read86rom(addr32);
-}
-
-static inline uint16_t read86rom16(uint32_t addr32) {
-    if (addr32 >= 0xFE000UL && addr32 <= 0xFFFFFUL) {
-        // BIOS ROM range
-        return read16arr(pBIOS(), 0xFE000UL, addr32);
-    }
-    if (addr32 >= 0xF6000UL && addr32 < 0xFA000UL) {
-        // IBM BASIC ROM LOW
-        return read16arr(pBASICL(), 0xF6000U, addr32);
-    }
-    if ((addr32 >= 0xFA000UL) && (addr32 < 0xFE000UL)) {
-        // IBM BASIC ROM HIGH
-        return read16arr(pBASICH(), 0xFA000UL, addr32);
-    }
-    return 0;
+    return *(pBIOS() + addr32 - 0xFE000UL);
 }
 
 static uint16_t read16umb_psram(uint32_t addr32) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         return read16psram(addr32);
     }
-    return read86rom16(addr32);
+    return read16arr(pBIOS(), 0xFE000UL, addr32);
 }
 
 static uint16_t read16umb_swap(uint32_t addr32) {
-    if (addr32 <= 0xFE000UL) {
+    if (addr32 < 0xFE000UL) {
         return ram_page_read16(addr32);
     }
-    return read86rom16(addr32);
+    return read16arr(pBIOS(), 0xFE000UL, addr32);
 }
 
 uint16_t umb_allocate(uint16_t* psz, uint16_t* err) {
